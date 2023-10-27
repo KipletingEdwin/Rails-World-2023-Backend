585ea289259ec6bfea234cd377bb04efe8ef23ea
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-10-27 20:14:49 +0300

    add the four methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,49 @@
 class ArticlesController < ApplicationController
+rescue_from ActiveRecord: :RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
         articles = Article.all
+        render json: articles, status: :ok
     end
 
     def show
-        article = Articles.find(params[:id])
+        article = find_article
+        render json: article
     end
 
-    def new
-        article = Article.new
+    def create
+        article = Article.create(article_params)
+        render json: article, status: :created
+    end
+
+    def update
+        article = find_article
+        article.update(article_params)
+        render json: article, status: :ok
+    end
+
+    def destroy
+        article = find_article
+        article.destroy
+        head :no_content
+    end
+
+
+
+
+
+    private
+
+    def find_article
+        Article.find(params[:id])
+    end
+
+    def article_params
+        params.permit(:firstname, :lastname)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+
     end
 end
